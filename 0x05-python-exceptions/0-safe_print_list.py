bf41809a92e4207aaa79cd6e39b11de6400d8907
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-10-10 05:56:42 -0700

    function that print list element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end='')
-            counter = counter + 1
+            counter += 1
         except:
             break
-        print('')
-        return counter
+    print('')
+    return counter
