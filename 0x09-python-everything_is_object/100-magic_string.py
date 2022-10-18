43a6625ce125e46d007d2e2b024555c08208399e
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-10-18 13:00:54 -0700

    Magic String

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(string=[]):
-    string += [“BestSchool”]
-    return (", ".join(string))
+def magic_string(count=[-1]):
+    count[0] += 1
+    return “BestSchool” + “, BestSchool” * count[0]
