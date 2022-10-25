f0eb784bc0bbc9f3846cdcd55de82a334e6026dc
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-10-25 13:26:31 -0700

    Student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,6 +11,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
         if attrs is None:
             return (self.__dict__)
         else:
@@ -19,3 +20,9 @@ class Student:
                 if hasattr(self, nm):
                     dic[nm] = getattr(self, nm)
             return (dic)
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance"""
+        save = vars(self)
+        for key, value in json.items():
+            save[key] = value
