244e5b9d326ea79f1a527b79a91dcffcb031e8c8
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-11-30 13:11:24 -0800

    database to lists all genres of the show

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- script that lists all genre from the database
 
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_show_genres
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_show_genres.genre_id ORDER BY number_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+INNER JOIN tv_show_genres AS tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
