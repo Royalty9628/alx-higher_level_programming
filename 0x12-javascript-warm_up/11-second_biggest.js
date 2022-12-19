4b56bc010bd0ad9fdecf06b5160ce1e3424d93b9
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-12-19 11:13:51 -0800

    My Object

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,9 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const list = process.argv
+	.map(Number)
+	.slice(2, process.argv.length)
+	.sort((a, b) => a - b);
+  console.log(list[list.length - 2]);
 }
