24bb32920e8e3f7856a40443ab6b72e62ca96bc6
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-12-19 10:52:37 -0800

    Addition of Two Integers

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-const Sqa = Number(process.argv[2]);
-
-if (Number(process.argv[2])) {
-
-  for (let i = 0; i < Sqa; i++) {
-    console.log('X'.repeat(Sqa));
-  }
-} else {
+if (isNaN(process.argv[2])) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
 }
