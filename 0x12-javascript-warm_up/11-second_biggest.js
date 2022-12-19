ef9ac6bd14fb0f2a50eb743007c34e1532f88efe
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-12-19 11:19:07 -0800

    Add Function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv
-	.map(Number)
-	.slice(2, process.argv.length)
-	.sort((a, b) => a - b);
-  console.log(list[list.length - 2]);
+  const args = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
