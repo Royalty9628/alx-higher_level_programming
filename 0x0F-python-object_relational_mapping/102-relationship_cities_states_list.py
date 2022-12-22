c969de84a0c5986c2a955c0e7d26ac10985a6411
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-12-21 16:03:05 -0800

    List all city object

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -4,11 +4,11 @@ Module that Lists all State objects, and
 corresponding City objects through Alchemy from the database
 """
 
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
-import sys
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
@@ -16,12 +16,11 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for a_city in session.query(City).order_by(City.id):
-            print("{}: {} -> {}".format(a_city.id, a_city.name,
-                  a_city.state.name))
+    for state in session.query(State):
+        for city in state.cities:
+            print("{:d}: {} -> {}".format(city.id, city.name, state.name))
+
     session.close()
