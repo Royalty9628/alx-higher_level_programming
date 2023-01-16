b99a00fc5a000abf8ecdfeff659ea78565a40a1c
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2023-01-16 08:36:17 -0800

    only status code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,19 +5,23 @@
 
 def find_peak(list_of_integers):
 
-    size = list_of_integers
-    if size == []:
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
-    total = len(loi)
-    if total == 1:
-        return size[0]
-    elif total == 2:
-        return max(size)
-    middle = int(total / 2)
-    peak = size[middle]
-    if peak > size[middle - 1] and peak > size[middle + 1]:
-        return peak
-    elif peak < size[middle - 1]:
-        return find_peak(size[:middle])
-    else:
-        return find_peak(size[middle + 1:])
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
