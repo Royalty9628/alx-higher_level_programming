f239ec61b6b4ffac8562f1d09c3b63b472c10034
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2023-01-16 07:09:19 -0800

    fix error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, send a request to that URL
-# and displays the size of the body of the response
+# displays the size of the body of the response
 curl -sI "$1" | grep "Content-Length" | cut -d' ' -f 2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL
-# And displays the body of the response
-curl -Ls "$1"
+# sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
